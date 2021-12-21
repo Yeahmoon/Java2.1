71569894a1f24a199dd1d00e8fc23f216b74ed3f
Ekaterina Khalillulina <yeahmoon@hotmail.com>
2021-12-21 18:04:37 +0300

    Java2.1.iml added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int Price = 19;
+        int MinMilesLimit = 20;
+        int TotalAmount = Price/MinMilesLimit;
+
+        System.out.println(TotalAmount);
+
+    }
+}
